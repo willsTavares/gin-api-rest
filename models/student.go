fb5b8f6777256f3080fad2c227f607def42d9aff
Willians Tavares <willians.tavares@konia.com.br>
2024-07-07 09:53:05 -0300

    fix: Add regex fo rg and cpf

--- models/student.go
+++ models/student.go
@@ -9,8 +9,8 @@ import (
 type Student struct {
 	gorm.Model
 	Name string `json:"name" validate:"nonzero"`
-	CPF  string `json:"cpf" validate:"len=11"`
-	RG   string `json:"rg" validate:"len=9"`
+	CPF  string `json:"cpf" validate:"len=11, regexp=^[0-11]*$"`
+	RG   string `json:"rg" validate:"len=9, regexp=^[0-9]*$"`
 }
 
 var Students []Student
